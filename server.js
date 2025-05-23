0f57388fbc92a42e1e0b398c6e2a7e3c4ccf5338
Narendra Kaushik <156305888+Narendra0502@users.noreply.github.com>
2025-05-23 06:20:53 +0530

    handling cors

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/meralinksaver')
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
