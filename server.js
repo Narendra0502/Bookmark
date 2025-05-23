8ea3330fc33b6676dff7b8d0867b264f56c9c688
Narendra Kaushik <156305888+Narendra0502@users.noreply.github.com>
2025-05-23 06:20:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://bookmark-topaz-phi.vercel.app'],
   credentials: true
 }));
 app.use(cookieParser());
